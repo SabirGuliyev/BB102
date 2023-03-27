c13dc39a6e4ae02b8f7c9325bd49ac54625093dd
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-03-27 21:48:53 +0400

    C# intro added

---
+++ 1.C# intro/bb102First/bb102First/Program.cs
@@ -0,0 +1,43 @@
+﻿string[] names = { "Fidan", "Revan", "Azade" };
+
+//CTRL+k+u
+//for (int i = 0; i < names.Length; i++)
+//{
+//    if (names[])
+//    {
+
+//    }
+//    Console.WriteLine(names[i]);
+//}
+
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+for (int i = 0; i < 10; i++)
+{
+    if (i%2!=0)
+    {
+        continue;
+    }
+
+    Console.WriteLine(i);
+    
+}
+
+
+
+//int i = 0;
+//while (i<10)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+//do
+//{
+
+//    Console.WriteLine("Salam");
+
+//} while (age>20);
