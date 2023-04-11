7fbd8ecbdf2e1fc638d047d7059a3c41c1902137
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-04-11 15:52:35 +0400

    Exception added

---
+++ 13.ExceptionReflection/ExceptionReflection/Program.cs
@@ -0,0 +1,123 @@
+﻿using ExceptionReflection.Utilities.Exceptions;
+
+namespace ExceptionReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Exception Try Catch
+            //try
+            //{
+            //    string name = "Revan";
+            //    int a = int.Parse(Console.ReadLine());
+            //    int b = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine(a / b);
+            //    Console.WriteLine(name[10]);
+            //}
+            //catch (DivideByZeroException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //    Console.WriteLine("0 ra bolmek olmur ezizim");
+            //}
+            //catch (FormatException e)
+            //{
+            //    Console.WriteLine("Gunah sene guvenendedi");
+            //    int a;
+            //    string str = Console.ReadLine();
+            //    int.TryParse(str, out a);
+
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Qaqa bunu mende gozlemirdim");
+            //    Console.WriteLine(e.Message);
+            //} 
+            #endregion
+
+
+            string[] names = { "Azade", "Fidan", "Revan", "Yusif" };
+
+
+
+            //try
+            //{
+            //    if (IsExist(names, "Natiq"))
+            //    {
+            //        Console.WriteLine("Bele shexs movcuddur");
+            //    }
+            //}
+            //catch (NotFoundException e)
+            //{
+
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //Console.WriteLine("Trydan sonraki kod setirleri");
+
+
+
+           
+            try
+            {
+                Student student = new Student("al");
+                Console.WriteLine(student.Name);
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Vacib kodlar ishe dushdu");
+            }
+
+            Console.WriteLine("Program cs deki kod");
+               
+   
+
+
+        }
+
+        public static bool IsExist(string[] arr, string search)
+        {
+            
+
+            foreach (string item in arr)
+            {
+                if (item.ToLower().Trim() == search.ToLower().Trim())
+                {
+                   
+                   return true;
+                }
+            }
+            throw new NotFoundException($"{search} bu sehxs tapilmadi");
+        }
+
+
+
+
+
+
+
+
+
+        //public static bool IsExist(string[] arr,string search)
+        //{
+        //    bool result= false;
+
+        //    foreach (string item in arr)
+        //    {
+        //        if (item.ToLower().Trim()==search.ToLower().Trim())
+        //        {
+        //            result = true;
+        //            break;
+        //        }
+        //    }
+        //    return result;
+        //}
+    }
+}
---
+++ 13.ExceptionReflection/ExceptionReflection/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionReflection
+{
+    internal class Student
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set {
+
+                if (value.Trim().Length>=3)
+                {
+                    _name = value.Trim();
+                }
+                else
+                {
+                    throw new Exception("Name deyeri yanlish oldugu ucun program dayandirildi");
+                }
+
+
+            }
+               
+        }
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
