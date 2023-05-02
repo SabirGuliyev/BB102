fba2d24a9dae155af48ac4483d80b7b7b38d2940
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-05-02 15:46:58 +0400

    EF ORM added

---
+++ 21.EFStep/EFStep/Program.cs
@@ -0,0 +1,64 @@
+﻿using EFStep.DAL;
+using EFStep.Models;
+
+namespace EFStep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AppDbContext dbContext = new AppDbContext();
+
+            //ADD
+            //Student student = new Student
+            //{
+
+
+            //    Surname = "Baratli",
+            //    Name = "Fidan",
+            //    Age = 21
+            //};
+
+
+            //dbContext.Students.Add(student);
+            //dbContext.SaveChanges();
+
+
+            //Student existed = dbContext.Students.Find(4);
+            //existed.Name = "Ismayil3";
+            //dbContext.SaveChanges();
+
+            //Console.WriteLine(existed.Name+" "+existed.Surname);
+
+            //if (existed != null)
+            //{
+            //    dbContext.Students.Remove(existed);
+            //    dbContext.SaveChanges();
+            //}
+
+
+            List<Student> students = dbContext.Students.ToList();
+
+
+            // foreach (var item in students)
+            // {
+            //     Console.WriteLine(item.Name);
+            // }
+
+
+
+            //List<Student> searchedStudents=dbContext.Students.Where(s=>s.Age>20).ToList();
+
+
+
+            //foreach (var item in searchedStudents)
+            //{
+            //    Console.WriteLine(item.Name+" "+item.Age);
+            //}
+
+
+
+        }
+    }
+}
