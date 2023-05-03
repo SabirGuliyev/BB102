b51eac15522ca271e0a4384d2bafd8210e7348c0
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-05-03 15:40:48 +0400

    MVC intro added

---
+++ 22.BackFirstStep/BackFirstStep/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using BackFirstStep.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BackFirstStep.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Id=1,Name="Revan",Surname="Isgenderov"});
+            students.Add(new Student { Id=2,Name="Yusif",Surname="Aliyev"});
+
+
+
+
+            //1.ViewBag.Students=students;
+
+            //2.ViewData["Students"]=students;
+
+            TempData["Students"]=students;
+
+
+            
+
+
+            return RedirectToAction("About");
+
+
+            //return RedirectToAction("Index","Product");
+        }
+
+        public IActionResult About()
+        {
+            //return View("Test");
+          
+            //foreach (var item in students)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+            return View();
+        }
+    }
+}
