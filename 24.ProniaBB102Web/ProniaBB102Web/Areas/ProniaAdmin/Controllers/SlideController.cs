98a985e864d83d9b9c04246a4256cb85635f5174
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-05-11 08:17:28 +0400

    FileUpload added

--- 24.ProniaBB102Web/ProniaBB102Web/Areas/ProniaAdmin/Controllers/SlideController.cs
+++ 24.ProniaBB102Web/ProniaBB102Web/Areas/ProniaAdmin/Controllers/SlideController.cs
@@ -1,16 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProniaBB102Web.DAL;
+using ProniaBB102Web.Models;
 
 namespace ProniaBB102Web.Areas.ProniaAdmin.Controllers
 {
+    [Area("ProniaAdmin")]
     public class SlideController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public SlideController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slide> slides = await _context.Slides.ToListAsync();
+
+            return View(slides);
         }
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Slide slide)
+        {
+            if (slide.Photo == null)
+            {
+                ModelState.AddModelError("Photo", "Shekil hissesi bosh ola bilmez");
+                return View();
+            }
+
+            if (!slide.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "Gonderilen file-nin tipi uygun deyil");
+                return View();
+            }
+            if (slide.Photo.Length  > 200*1024)
+            {
+                ModelState.AddModelError("Photo", "Gonderilen file-nin hecmi 200 kb-den boyuk olmamalidir");
+                return View();
+            }
+
+            FileStream file = new FileStream(@"C:\Users\sabir\Desktop\BB102 git\24.ProniaBB102Web\ProniaBB102Web\wwwroot\assets\images\website-images\" + slide.Photo.FileName,FileMode.Create);
+            await slide.Photo.CopyToAsync(file);
+
+
+            slide.Image = slide.Photo.FileName;
+
+            await _context.Slides.AddAsync(slide);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+
+
+
+
+
+
+
+
+
         }
         public IActionResult Update()
         {
--- 24.ProniaBB102Web/ProniaBB102Web/Models/Slide.cs
+++ 24.ProniaBB102Web/ProniaBB102Web/Models/Slide.cs
@@ -1,14 +1,21 @@
-﻿namespace ProniaBB102Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProniaBB102Web.Models
 {
     public class Slide
     {
         public int Id { get; set; }
         public string Image { get; set; }
+        [Required]
         public string Title { get; set; }
         public string SubTitle { get; set; }
         public string Description { get; set; }
 
         public int Order { get; set; }
+
+        [NotMapped]
+        public IFormFile Photo { get; set; }
 
     }
 }
